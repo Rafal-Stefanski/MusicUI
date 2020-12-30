27008b015dd5dc2927220cdc6379f5ee4b28c61c
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-30 13:36:54 +0100

    358. Add ProgressBar (M)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TableView;
 import sample.model.Album;
 import sample.model.Artist;
@@ -16,9 +17,18 @@ public class Controller {
     @FXML
     private TableView artistTable; // be removing <Artist> from TableView<Artist>, makes the thing work (lesson 357)
 
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
     public void listArtists() {
         Task<ObservableList<Artist>> task = new GetAllArtistsTask();
         artistTable.itemsProperty().bind(task.valueProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
+
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(e -> progressBar.setVisible(false));
+        task.setOnFailed(e -> progressBar.setVisible(false));
 
         new Thread(task).start();
     }
